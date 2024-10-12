2b1c6f23c712b68f9d6d499ec1838be11fea7d6a
pks9862728888 <pks9862728888@gmail.com>
2024-10-12 11:23:19 +0530

    Added config server

--- rbac-ecommerce-config-server/src/main/java/com/demo/rbac/ecommerce/configserver/AppRbacEcommerceConfigServer.java
+++ rbac-ecommerce-config-server/src/main/java/com/demo/rbac/ecommerce/configserver/AppRbacEcommerceConfigServer.java
@@ -2,12 +2,14 @@ package com.demo.rbac.ecommerce.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class AppRbacEcommerceConfigServer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppRbacEcommerceConfigServer.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppRbacEcommerceConfigServer.class, args);
+    }
 
 }
