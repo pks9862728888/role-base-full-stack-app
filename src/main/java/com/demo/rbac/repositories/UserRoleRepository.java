29cf2350ba7b8f5eae041d63f9bf264060c92724
pks9862728888 <pks9862728888@gmail.com>
2024-10-12 12:10:03 +0530

    Deleted non-required folder

--- src/main/java/com/demo/rbac/repositories/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.rbac.repositories;
-
-import com.demo.rbac.entities.users.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-}
