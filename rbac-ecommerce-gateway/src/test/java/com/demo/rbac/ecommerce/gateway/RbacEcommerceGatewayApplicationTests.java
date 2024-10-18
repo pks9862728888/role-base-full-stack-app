775fbf2201b026778aad758150b696f4c2a0ccf0
pks9862728888 <pks9862728888@gmail.com>
2024-10-18 10:28:14 +0530

    Added gateway server

---
+++ rbac-ecommerce-gateway/src/test/java/com/demo/rbac/ecommerce/gateway/RbacEcommerceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.rbac.ecommerce.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacEcommerceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
